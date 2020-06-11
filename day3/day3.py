daac019399b441708526dc9ac28f2c98b9141061
Neil P <50508228+neilspace@users.noreply.github.com>
2020-06-11 21:19:28 +0530

    Update day3.py

--- day3/day3.py
+++ day3/day3.py
@@ -12,6 +12,6 @@ if __name__ == '__main__':
     N = int(input())
     if(N%2==1) or (N%2==0 and (N>=6 and N<=20 )) :
        print("Weird")
-    if N%2==0 and ((N>=2 and N<=5) or (N > 20)):
+    else:
        print("Not Weird")
     
